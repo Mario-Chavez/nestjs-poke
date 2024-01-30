5b78037fb121654bdf596a04c14dc9e4a907701d
Mario-Chavez <marioachavez1@gmail.com>
2024-01-30 10:42:21 -0300

    antes de la isercion en seed

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+    data.results.forEach(({ name, url }) => {
+      const segmentos = url.split('/'); //cortamos el url
+      const no = +segmentos[segmentos.length - 2]; //extraemos el penultima posicion donde esta en numero
+    });
+
+    return data.results;
+  }
+}
